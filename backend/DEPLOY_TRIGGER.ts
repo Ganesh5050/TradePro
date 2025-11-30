d63408dd321f80064e4e06e6ea166283a9bc21b8
Ganesh5050 <ganesh5050@example.com>
2025-11-30 13:22:26 +0530

    URGENT: Force deploy with new trigger file - Render stuck on old commit

---
+++ backend/DEPLOY_TRIGGER.ts
@@ -0,0 +1,3 @@
+console.log('🔥 Force deployment trigger - new file created!');
+console.log('This should force Render to deploy the latest commit');
+export const DEPLOY_TRIGGER = 'FORCE_DEPLOY_1.0.2';
